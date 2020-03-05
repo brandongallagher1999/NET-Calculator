9054ab0453dfcfea50d73506856bc4a530ae65a2
Brandon Gallagher <brandonegallagher@gmail.com>
2020-03-04 19:10:27 -0500

    Initial commit and final push.

---
+++ 200454237A1/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace _200454237A1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_total = new System.Windows.Forms.Label();
+            this.lbl_paid = new System.Windows.Forms.Label();
+            this.lbl_change = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.txt_paid = new System.Windows.Forms.TextBox();
+            this.txt_change = new System.Windows.Forms.TextBox();
+            this.lbl_toonies = new System.Windows.Forms.Label();
+            this.lbl_loonies = new System.Windows.Forms.Label();
+            this.lbl_quarters = new System.Windows.Forms.Label();
+            this.lbl_dimes = new System.Windows.Forms.Label();
+            this.lbl_nickels = new System.Windows.Forms.Label();
+            this.txt_toonies = new System.Windows.Forms.TextBox();
+            this.txt_loonies = new System.Windows.Forms.TextBox();
+            this.txt_quarters = new System.Windows.Forms.TextBox();
+            this.txt_dimes = new System.Windows.Forms.TextBox();
+            this.txt_nickels = new System.Windows.Forms.TextBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_total
+            // 
+            this.lbl_total.AutoSize = true;
+            this.lbl_total.Location = new System.Drawing.Point(17, 17);
+            this.lbl_total.Name = "lbl_total";
+            this.lbl_total.Size = new System.Drawing.Size(40, 17);
+            this.lbl_total.TabIndex = 0;
+            this.lbl_total.Text = "Total";
+            // 
+            // lbl_paid
+            // 
+            this.lbl_paid.AutoSize = true;
+            this.lbl_paid.Location = new System.Drawing.Point(131, 17);
+            this.lbl_paid.Name = "lbl_paid";
+            this.lbl_paid.Size = new System.Drawing.Size(36, 17);
+            this.lbl_paid.TabIndex = 1;
+            this.lbl_paid.Text = "Paid";
+            // 
+            // lbl_change
+            // 
+            this.lbl_change.AutoSize = true;
+            this.lbl_change.Location = new System.Drawing.Point(215, 17);
+            this.lbl_change.Name = "lbl_change";
+            this.lbl_change.Size = new System.Drawing.Size(57, 17);
+            this.lbl_change.TabIndex = 2;
+            this.lbl_change.Text = "Change";
+            // 
+            // txt_total
+            // 
+            this.txt_total.Location = new System.Drawing.Point(20, 47);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(70, 22);
+            this.txt_total.TabIndex = 3;
+            // 
+            // txt_paid
+            // 
+            this.txt_paid.Location = new System.Drawing.Point(111, 47);
+            this.txt_paid.Name = "txt_paid";
+            this.txt_paid.Size = new System.Drawing.Size(76, 22);
+            this.txt_paid.TabIndex = 4;
+            // 
+            // txt_change
+            // 
+            this.txt_change.Location = new System.Drawing.Point(218, 47);
+            this.txt_change.Name = "txt_change";
+            this.txt_change.ReadOnly = true;
+            this.txt_change.Size = new System.Drawing.Size(68, 22);
+            this.txt_change.TabIndex = 5;
+            // 
+            // lbl_toonies
+            // 
+            this.lbl_toonies.AutoSize = true;
+            this.lbl_toonies.Location = new System.Drawing.Point(20, 123);
+            this.lbl_toonies.Name = "lbl_toonies";
+            this.lbl_toonies.Size = new System.Drawing.Size(59, 17);
+            this.lbl_toonies.TabIndex = 6;
+            this.lbl_toonies.Text = "Toonies";
+            // 
+            // lbl_loonies
+            // 
+            this.lbl_loonies.AutoSize = true;
+            this.lbl_loonies.Location = new System.Drawing.Point(108, 123);
+            this.lbl_loonies.Name = "lbl_loonies";
+            this.lbl_loonies.Size = new System.Drawing.Size(58, 17);
+            this.lbl_loonies.TabIndex = 7;
+            this.lbl_loonies.Text = "Loonies";
+            // 
+            // lbl_quarters
+            // 
+            this.lbl_quarters.AutoSize = true;
+            this.lbl_quarters.Location = new System.Drawing.Point(208, 123);
+            this.lbl_quarters.Name = "lbl_quarters";
+            this.lbl_quarters.Size = new System.Drawing.Size(64, 17);
+            this.lbl_quarters.TabIndex = 8;
+            this.lbl_quarters.Text = "Quarters";
+            // 
+            // lbl_dimes
+            // 
+            this.lbl_dimes.AutoSize = true;
+            this.lbl_dimes.Location = new System.Drawing.Point(302, 123);
+            this.lbl_dimes.Name = "lbl_dimes";
+            this.lbl_dimes.Size = new System.Drawing.Size(47, 17);
+            this.lbl_dimes.TabIndex = 9;
+            this.lbl_dimes.Text = "Dimes";
+            // 
+            // lbl_nickels
+            // 
+            this.lbl_nickels.AutoSize = true;
+            this.lbl_nickels.Location = new System.Drawing.Point(392, 123);
+            this.lbl_nickels.Name = "lbl_nickels";
+            this.lbl_nickels.Size = new System.Drawing.Size(53, 17);
+            this.lbl_nickels.TabIndex = 10;
+            this.lbl_nickels.Text = "Nickels";
+            // 
+            // txt_toonies
+            // 
+            this.txt_toonies.Location = new System.Drawing.Point(20, 173);
+            this.txt_toonies.Name = "txt_toonies";
+            this.txt_toonies.ReadOnly = true;
+            this.txt_toonies.Size = new System.Drawing.Size(70, 22);
+            this.txt_toonies.TabIndex = 11;
+            // 
+            // txt_loonies
+            // 
+            this.txt_loonies.Location = new System.Drawing.Point(111, 173);
+            this.txt_loonies.Name = "txt_loonies";
+            this.txt_loonies.ReadOnly = true;
+            this.txt_loonies.Size = new System.Drawing.Size(76, 22);
+            this.txt_loonies.TabIndex = 12;
+            // 
+            // txt_quarters
+            // 
+            this.txt_quarters.Location = new System.Drawing.Point(211, 173);
+            this.txt_quarters.Name = "txt_quarters";
+            this.txt_quarters.ReadOnly = true;
+            this.txt_quarters.Size = new System.Drawing.Size(75, 22);
+            this.txt_quarters.TabIndex = 13;
+            // 
+            // txt_dimes
+            // 
+            this.txt_dimes.Location = new System.Drawing.Point(305, 173);
+            this.txt_dimes.Name = "txt_dimes";
+            this.txt_dimes.ReadOnly = true;
+            this.txt_dimes.Size = new System.Drawing.Size(70, 22);
+            this.txt_dimes.TabIndex = 14;
+            // 
+            // txt_nickels
+            // 
+            this.txt_nickels.Location = new System.Drawing.Point(395, 173);
+            this.txt_nickels.Name = "txt_nickels";
+            this.txt_nickels.ReadOnly = true;
+            this.txt_nickels.Size = new System.Drawing.Size(68, 22);
+            this.txt_nickels.TabIndex = 15;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(20, 210);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(85, 32);
+            this.btn_calculate.TabIndex = 16;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(378, 210);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(85, 32);
+            this.btn_clear.TabIndex = 17;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 259);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.txt_nickels);
+            this.Controls.Add(this.txt_dimes);
+            this.Controls.Add(this.txt_quarters);
+            this.Controls.Add(this.txt_loonies);
+            this.Controls.Add(this.txt_toonies);
+            this.Controls.Add(this.lbl_nickels);
+            this.Controls.Add(this.lbl_dimes);
+            this.Controls.Add(this.lbl_quarters);
+            this.Controls.Add(this.lbl_loonies);
+            this.Controls.Add(this.lbl_toonies);
+            this.Controls.Add(this.txt_change);
+            this.Controls.Add(this.txt_paid);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.lbl_change);
+            this.Controls.Add(this.lbl_paid);
+            this.Controls.Add(this.lbl_total);
+            this.Name = "Form1";
+            this.Text = "frm_main";
+            this.Load += new System.EventHandler(this.frm_main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_total;
+        private System.Windows.Forms.Label lbl_paid;
+        private System.Windows.Forms.Label lbl_change;
+        private System.Windows.Forms.TextBox txt_total;
+        private System.Windows.Forms.TextBox txt_paid;
+        private System.Windows.Forms.TextBox txt_change;
+        private System.Windows.Forms.Label lbl_toonies;
+        private System.Windows.Forms.Label lbl_loonies;
+        private System.Windows.Forms.Label lbl_quarters;
+        private System.Windows.Forms.Label lbl_dimes;
+        private System.Windows.Forms.Label lbl_nickels;
+        private System.Windows.Forms.TextBox txt_toonies;
+        private System.Windows.Forms.TextBox txt_loonies;
+        private System.Windows.Forms.TextBox txt_quarters;
+        private System.Windows.Forms.TextBox txt_dimes;
+        private System.Windows.Forms.TextBox txt_nickels;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ 200454237A1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _200454237A1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private decimal total;
+        private decimal paid;
+        private decimal change;
+        private Int64 toonies;
+        private Int64 loonies;      //HUGE NUMBERS POSSIBLE SO INT64
+        private Int64 quarters;
+        private Int64 dimes;
+        private Int64 nickels;
+
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+            this.total = 0m;
+            this.paid = 0m;
+            this.change = 0m;
+        }
+
+        private void calculate_coins()
+        {
+            /*Calculates every coin and their respective amounts.
+             * 
+             * :args -> None
+             * 
+             * :returns -> void
+             */
+
+            decimal temp = change;
+
+            this.toonies = Convert.ToInt64(Math.Floor(temp/2)); // 17.38 / 2 = 8
+            temp -= toonies * 2; // 17.38 - 16 = 1.38
+
+            this.loonies = Convert.ToInt64(Math.Floor(temp / 1)); //1.38 / 1 = 1
+            temp -= loonies * 1;    //1.38 - 1 = 0.38
+
+            this.quarters = Convert.ToInt64(Math.Floor(temp / 0.25m)); // 0.38 / 0.25 = 1
+            temp -=  quarters * 0.25m;    // 0.38 - 0.25 = 0.13
+
+            this.dimes = Convert.ToInt64(Math.Floor(temp / 0.10m));
+            temp -= quarters * 0.10m; // 0.3
+
+            this.nickels = Convert.ToInt64(Math.Floor(temp / 0.05m));
+            temp -= quarters * 0.05m;
+
+            txt_toonies.Text = this.toonies.ToString();
+            txt_loonies.Text = this.loonies.ToString();
+            txt_quarters.Text = this.quarters.ToString();
+            txt_dimes.Text = this.dimes.ToString();
+            txt_nickels.Text = this.nickels.ToString();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            /*
+             * When the Calculate button is clicked, this handler is called.
+             *
+             *:arg sender (object)
+             *:arg e (EventArgs)
+             * 
+             * :returns -> None
+             */
+            this.total = Convert.ToDecimal(txt_total.Text);
+            this.paid = Convert.ToDecimal(txt_paid.Text);
+            this.change = paid - total;
+            txt_change.Text = change.ToString(); // need to typecast to string cause it wont take int type
+            calculate_coins();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            /*Resets the boxes and all values.
+             * 
+             * 
+             */
+            this.paid = 0m;
+            this.total = 0m;
+            this.change = 0m;
+            this.toonies = 0;
+            this.loonies = 0;
+            this.quarters = 0;
+            this.dimes = 0;
+            this.nickels = 0;
+
+            txt_paid.Text = "";
+            txt_total.Text = "";
+            txt_change.Text = "";
+            txt_toonies.Text = "";
+            txt_loonies.Text = "";
+            txt_quarters.Text = "";
+            txt_dimes.Text = "";
+            txt_nickels.Text = "";
+        }
+    }
+}
